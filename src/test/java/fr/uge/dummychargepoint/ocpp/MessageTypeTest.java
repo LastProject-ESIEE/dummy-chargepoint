9a0b29b69006170508011ac43c04abe8b4cdf695
Thomas VELU <velu.thomas@murena.io>
2025-07-29 19:59:23 +0200

    test: add test file for initializing test

--- src/test/java/fr/uge/dummychargepoint/ocpp/MessageTypeTest.java
+++ src/test/java/fr/uge/dummychargepoint/ocpp/MessageTypeTest.java
@@ -1,13 +1,17 @@
 package fr.uge.dummychargepoint.ocpp;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MessageTypeTest {
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MessageTypeTest {
 
   @Test
-  public void should_return_unknown() {
+  void should_return_unknown() {
     // given
     var code = 10;
 
@@ -16,6 +20,26 @@ public class MessageTypeTest {
 
     // then
     assertThat(messageType).isEqualTo(MessageType.UNKNOWN);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCorrectEnum")
+  void should_return_correct_enum(int callType, MessageType expected) {
+    // given
+    // Here we have the callType parameter which we are going to use below
+    // when
+    var messageType = MessageType.from(callType);
+
+    // then
+    assertThat(messageType).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> provideCorrectEnum() {
+    return Stream.of(
+      Arguments.of(2, MessageType.REQUEST),
+      Arguments.of(3, MessageType.RESPONSE),
+      Arguments.of(-1, MessageType.UNKNOWN)
+    );
   }
 
 }
