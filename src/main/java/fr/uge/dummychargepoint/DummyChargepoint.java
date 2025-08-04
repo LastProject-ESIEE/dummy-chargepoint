616628a811c4884d3c66d8913867ffde8f32f66d
Thomas VELU <velu.thomas@murena.io>
2025-08-04 22:32:26 +0200

    feat: use main as a way to test communication

--- src/main/java/fr/uge/dummychargepoint/DummyChargepoint.java
+++ src/main/java/fr/uge/dummychargepoint/DummyChargepoint.java
@@ -1,9 +1,32 @@
 package fr.uge.dummychargepoint;
 
-import fr.uge.dummychargepoint.application.Application;
+import fr.uge.dummychargepoint.socket.Application;
+
+import java.net.URISyntaxException;
+import java.util.Locale;
 
 public class DummyChargepoint {
-  public static void main(String[] args) {
-    new Application().launch();
+
+  public static void main(String[] args) throws URISyntaxException, InterruptedException {
+    if (args.length < 2) {
+      throw new AssertionError();
+    }
+    switch (args[0].toUpperCase(Locale.ROOT)) {
+      case "SERVER" -> {
+        var port = Integer.parseInt(args[1]);
+        var application = new Application(port);
+        application.start();
+      }
+      case "CLIENT" -> {
+        if (args.length < 4) {
+          throw new AssertionError();
+        }
+        var remoteHost = args[1];
+        var remotePort = Integer.parseInt(args[2]);
+        var localPort = Integer.parseInt(args[3]);
+        var application = new Application(remoteHost, remotePort, localPort);
+        application.connectBlocking();
+      }
+    }
   }
 }
---
+++ src/main/java/fr/uge/dummychargepoint/socket/Application.java
@@ -0,0 +1,36 @@
+package fr.uge.dummychargepoint.socket;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+public class Application {
+
+  private final boolean isServer;
+  private final ConfigurationApplication application;
+
+  public Application(int port) {
+    isServer = true;
+    application = new ConfigurationServer(new InetSocketAddress(port));
+  }
+
+  public Application(String remoteHost, int remotePort, int localPort) throws URISyntaxException {
+    isServer = false;
+    var serverUri = new URI("ws://" + remoteHost + ":" + remotePort);
+    application = new ConfigurationClient(serverUri, Map.of());
+  }
+
+  public void start() {
+    if (isServer) {
+      application.start();
+    }
+  }
+
+  public void connectBlocking() throws InterruptedException {
+    if (!isServer) {
+      application.connectBlocking();
+    }
+  }
+
+}
